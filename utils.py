4e4ef647c625ee5cadf1cb1e1bbedb670448ea7d
Pooja Rajegore <poojasawale9356@gmail.com>
2026-06-05 19:30:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from pathlib import Path
 
 @st.cache_data
 def load_data():
 
     file_path = (
-        Path(__file__).parent
+        Path(__file__).resolve().parent
         / "Data"
         / "Cleaned_Afficionado_Coffee_Data.xlsx"
     )
 
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(
+        file_path,
+        engine="openpyxl"
+    )
 
     return df
-
