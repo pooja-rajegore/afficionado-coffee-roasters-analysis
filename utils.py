e82940cb8de4963cd4b734b49dd6a09ee5728b43
Pooja Rajegore <poojasawale9356@gmail.com>
2026-06-05 19:18:30 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,12 +5,13 @@ from pathlib import Path
 @st.cache_data
 def load_data():
 
-    file_path = (
-        Path(__file__).parent
-        / "Data"
-        / "Cleaned_Afficionado_Coffee_Data.xlsx"
-    )
+    file_path = Path(__file__).resolve().parent / "Data" / "Cleaned_Afficionado_Coffee_Data.xlsx"
 
-    df = pd.read_excel(file_path)
+    st.write("File path:", file_path)
+    st.write("Exists:", file_path.exists())
 
-    return df
+    if not file_path.exists():
+        st.error(f"Missing file: {file_path}")
+        st.stop()
+
+    return pd.read_excel(file_path)
